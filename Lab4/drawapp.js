049f2efe27cee2a8cc34968f7c5d6ed6040a2adf
AnupSatyal <t4saan02@students.oamk.fi>
2019-03-28 00:42:24 +0200

    updated draw app

--- Lab4/drawapp.js
+++ Lab4/drawapp.js
@@ -1,52 +1,78 @@
+var canvas,
+    context,
+    dragging = false,
+    dragStartlocation,
+    snapshot;
 
-var canvas = document.getElementById('mycanvas');
-var c = canvas.getContext('2d');
-// setting canvas full screen 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
 
-c.strokeStyle = "#000000"
-// end of line is rounded
-c.lineJoin = "round";
-c.lineCap = "round";
-c.lineWidth = 5;
 
-var isDrawing = false;
-var lastX = 0;
-var lastY = 0;
+function getCanvasCoordinates(event) {
+	var x = event.clientX - canvas.getBoundingClientRect().left, 
+	    y = event.clientY - canvas.getBoundingClientRect().top;
 
-function draw(e) {
-
-	if(!isDrawing) {
-		return; //stop the function when mouse is not pressed
-
-	}
-	else{
-		console.log(e); 
-		c.beginPath();
-		c.moveTo(lastX, lastY);
-		c.lineTo(e.offsetX, e.offsetY);
-		c.stroke();
-
-		// updating last X and lastY
-
-		lastX = e.offsetX;
-		lastY = e.offsetY;
-	}
-	
+	return {x: x, y: y};
 }
 
-canvas.addEventListener('mousemove', draw);
-canvas.addEventListener('mousedown', (e) => {
-	isDrawing = true;
-	lastX = e.offsetX;
-	lastY = e.offsetY;
+function takeSnapshot() {
+	snapshot = context.getImageData(0, 0, canvas.width, canvas.height);
+}
 
-});
+
+function restoreSnapshot() {
+	context.putImageData(snapshot, 0, 0);
+}
+
+
+function drawLine(position) {
+  context.beginPath();
+  context.moveTo(dragStartlocation.x, dragStartlocation.y);
+  context.lineTo(position.x, position.y);
+  context.stroke();
+
+
+}
+
+
+function dragstart(event) {
+	dragging = true;
+	dragStartlocation = getCanvasCoordinates(event);
+	takeSnapshot();
+    
+}
+
+function drag(event) {
+     var position;
+     if(dragging == true) {
+     	restoreSnapshot();
+     	position = getCanvasCoordinates(event);
+     	drawLine(position);
+     }
+}
+
+function dragstop(event) {
+     dragging = false;
+     restoreSnapshot();
+     var position = getCanvasCoordinates(event);
+     drawLine(position);
+}
+ 
+function init() {
+     canvas = document.getElementById("canvas");
+     context = canvas.getContext('2d');
+     context.strokeStyle = 'black';
+     context.lineWidth = 4;
+     context.lineCap = 'round';
+
+     canvas.addEventListener('mousedown', dragstart, false);
+     canvas.addEventListener('mousemove', drag, false);
+     canvas.addEventListener('mouseup', dragstop, false);
+
+
+}
 
 function clearCanvas(){
-	c.clearRect(0,0, canvas.width, canvas.height);
+	context.clearRect(0,0, canvas.width, canvas.height);
 }
 
-canvas.addEventListener('mouseup', () => isDrawing = false);
-canvas.addEventListener('mouseout', () => isDrawing = false);
+
+window.addEventListener('load', init, false);
